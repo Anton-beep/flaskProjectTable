180a7a5d426cc861a259f377d32aaa4e832f9062
Anton-beep <anton.plyu@gmail.com>
2022-04-13 13:17:06 +0300

    info commit

--- data/sqlalchemy/__all_models.py
+++
@@ -1,3 +0,0 @@
-from ..models import users
-from ..models import lessons
-from ..models import replacements
--- forms/token_check.py
+++
@@ -1,8 +0,0 @@
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
-from flask_wtf import FlaskForm
-
-
-class TokenForm(FlaskForm):
-    token = StringField('Токен', validators=[DataRequired()])
-    submit = SubmitField('Продолжить')
