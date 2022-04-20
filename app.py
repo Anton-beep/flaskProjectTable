2e3ce55945f72fa1700ed8eca65943baa8119919
Anton-beep <anton.plyu@gmail.com>
2022-04-21 02:29:09 +0300

    heroku

--- app.py
+++ app.py
@@ -591,4 +591,6 @@ def user_edit(id):
                            image_path=image_path)
 
 
-app.run()
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
